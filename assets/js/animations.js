152ba388e77ac69021156fd9d89d0a697c62b1ff
AllvesMatteus <allves.matteus@hotmail.com>
2025-03-23 23:39:30 -0300

    first commit

---
+++ assets/js/animations.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".animate");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".from-left, .from-right");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
